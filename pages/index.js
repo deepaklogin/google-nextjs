5bd8294994c0debefda3b57ba9859b81c8d9f7ef
deepak <deepak.rajak@vkaps.com>
2021-09-02 02:22:47 +0530

    second

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Avatar from '../components/Avatar'
-import {MicrophoneIcon, ViewGridIcon} from '@heroicons/react/solid'
-import {SearchIcon} from '@heroicons/react/outline'
+import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
+import { SearchIcon } from '@heroicons/react/outline'
 import Footer from '../components/Footer'
 import { useRef } from 'react'
 import { useRouter } from 'next/router'
@@ -14,9 +14,9 @@ export default function Home() {
   const search = (e) => {
     e.preventDefault()
 
-    const term= searchInputRef.current.value;
+    const term = searchInputRef.current.value;
 
-    if(!term) return;
+    if (!term) return;
 
     router.push(`search?term=${term}`)
 
@@ -29,7 +29,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <header className="flex w-full p-5 justify-between text-sm text-gray-700">
         <div className="flex space-x-4 items-center">
           <p className="link">About</p>
@@ -39,13 +38,12 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
-          {/* Icon */}
-          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer"/>
-          {/* Avatar */}
-          <Avatar url='https://coaching.papareact.com/ai9'/>
+
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+
+          <Avatar url="https://coaching.papareact.com/ai9" />
         </div>
       </header>
-      {/* Body */}
 
       <form className="flex flex-col items-center mt-40 flex-grow w-4/5">
         <Image
@@ -55,9 +53,9 @@ export default function Home() {
           alt="google Image"
         />
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border-gray-200 border px-5 py-3 items-center sm:max-w-xl lg:max-w-xl">
-            <SearchIcon className="h-5 mr-3 text-gray-500"/>
-            <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none"/>
-            <MicrophoneIcon className="h-5" />
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input ref={searchInputRef} type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
         </div>
         <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
           <button className="btn" onClick={search}>Google Search</button>
@@ -66,9 +64,7 @@ export default function Home() {
         </div>
       </form>
 
-      {/* Footer */}
-
-      <Footer/>
+      <Footer />
     </div>
   )
 }
